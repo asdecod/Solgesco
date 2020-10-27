e34b6452783d30a1094f15a16aa00c386eda4464
justinho Mikala <mikalajuste@gmail.com>
2020-10-27 21:42:52 +0100

    index du serveur

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+header("refresh: 0; https://solgesco.profile-master.com/public/");
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="Asdecod">
+    <meta name="keywords" content="Asdecod, agence, agence web, web, gabon, libreville, web agency, professionnel, internet, justinho, mikala, justinho mikala, conception web, marketing web, referencement web, impact gabon">
+    <meta name="author" content="Asdecod">
+    <meta name="robots" content="index, follow" />
+    <meta name="twitter:site" content="twitter.com/TeamAsdecod" />
+    <meta name="facebook:site" content="facebook.com/asdecod.officiel" />
+    <title>Solution de gestion scolaire de Asdecod | Solgesco </title>
+    <link rel="canonical" href="https://www.asdecod.com/" />
+    <link rel="copyright" href="justinhomikala.xyz" title="Copyright" />
+    <link rel="icon" href="public/favicon.ico" type="image/x-icon" />
+	<meta name="description" content="Notre mission principale est de fournir des services de qualité à nos différents clients selon leurs besoins variés. Cela passe par: L&#039; Accompagnement des entreprises dans la création et la mise en valeur de leur visibilité en-ligne et sur le marché physique; .... Asdecod let&#039; s code the world together">
+    <meta property="og:title" content="Ameliorez votre visibilité sur internet avec Asdecod">
+    <meta property="og:description" content="Nous sommes une agence web spécialisée dans les services de communication et de marketing digitale, située à Libreville">
+    <meta property="og:image" content="https://asdecod.com/public/img/cover.png">
+    <meta property="og:url" content="https://asdecod.com/public/Accueil">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:facebook" content="Asdecod">
+    <meta name="twitter:image:alt" content="Asdecod">
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style>
+        .loader15{text-align:center;margin:40px 0}
+        .loader15 span{width:20px;height:20px;border-radius:50%;background:#f7bd3a;display:inline-block}
+        .loader15 span:first-child{animation:loading-152 .5s linear infinite;opacity:0;transform:translate(-20px)}
+        .loader15 span:nth-child(2),.loader15 span:nth-child(3){animation:loading-153 .5s linear infinite}
+        .loader15 span:last-child{animation:loading-15 .5s linear infinite}
+        @-webkit-keyframes loading-15{
+            100%{transform:translate(40px);opacity:0}
+        }
+        @keyframes loading-15{
+            100%{transform:translate(40px);opacity:0}
+        }
+        @-webkit-keyframes loading-152{
+            100%{transform:translate(20px);opacity:1}
+        }
+        @keyframes loading-152{
+            100%{transform:translate(20px);opacity:1}
+        }
+        @-webkit-keyframes loading-153{
+            100%{transform:translate(20px)}
+        }
+        @keyframes loading-153{
+            100%{transform:translate(20px)}
+        }
+
+    </style>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147215412-1"></script>
+</head>
+<body >
+
+
+    <div class="container mt-5">
+    <br/><br/><br/><br/>
+
+        <div align="center" class="text-center  mt-5">
+            <h3 align="center" class="h3"> Welcome</h3>
+
+            <div class="row">
+                <div class="col-md-12 mb-5">
+                    <div class="loader15">
+                        <span></span><span></span><span></span><span></span>
+                    </div>
+                </div>
+            </div><br/><br/><br/><br/>
+            <h6 align="center" class="mt-5"> Chargement...</h6>
+        </div>
+        
+        
+        
+    </div>
+
+</body>
+</html>
